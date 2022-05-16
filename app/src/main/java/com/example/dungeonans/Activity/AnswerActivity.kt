e005283bf6497c7887eb2b2372c9abb6904aaaa5
tnals123 <soo8608@naver.com>
2022-05-16 16:48:19 +0900

    update

--- app/src/main/java/com/example/dungeonans/Activity/AnswerActivity.kt
+++ app/src/main/java/com/example/dungeonans/Activity/AnswerActivity.kt
@@ -1,24 +1,27 @@
 package com.example.dungeonans.Activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dungeonans.Adapter.PostCommentCardViewAdapter
-import com.example.dungeonans.DataClass.PostCommentData
-import com.example.dungeonans.DataClass.comment_type1
-import com.example.dungeonans.DataClass.comment_type2
+import com.example.dungeonans.DataClass.*
 import com.example.dungeonans.R
-
+import com.example.dungeonans.Retrofit.RetrofitClient
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 
 // 답변은 답변만 보여주고, 답변의 점점점을 누르면 답변 밑에 달린 모든 댓글들 다 볼 수 있게 처리,,
@@ -34,15 +37,44 @@ class AnswerActivity : AppCompatActivity() {
     var commentItemCount = 0
     lateinit var recyclerView : RecyclerView
     lateinit var commentEditText: EditText
+    lateinit var askPostWebView : WebView
+    var askWebViewUrl = "file:///android_asset/ask_post.html"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.answer_fragment)
+        val posting_list: String? = intent.getStringExtra("posting_index")
+        renderWebView(posting_list!!.toInt())
+
+        askPostWebView = findViewById(R.id.askPostWebView)
+        askPostWebView.settings.javaScriptEnabled = true
+        askPostWebView.settings.domStorageEnabled = true
+        askPostWebView.settings.allowContentAccess = true
+        askPostWebView.settings.allowFileAccess = true
 
         commentEditText = findViewById(R.id.commentEditText)
         commentEditText.setOnClickListener{
             commentEditText.requestFocus()
         }
+
+        class WebBrideg(private val mContext: Context) {
+
+            @JavascriptInterface
+            fun showToast(code: String) {
+                Toast.makeText(mContext, code, Toast.LENGTH_SHORT).show()
+            }
+
+            @JavascriptInterface
+            fun getwidth(px: Float): Float {
+                var resources = resources
+                val metrics: DisplayMetrics = resources.getDisplayMetrics()
+                val dp = px * (DisplayMetrics.DENSITY_DEFAULT / metrics.densityDpi.toFloat())
+                return dp
+            }
+        }
+
+        askPostWebView.addJavascriptInterface(WebBrideg(this), "Android")
+
 
         // edittext 옆 버튼 이벤트 리스너
         var writeCommentBtn : ImageButton = findViewById(R.id.writeCommentBtn)
@@ -68,6 +100,32 @@ class AnswerActivity : AppCompatActivity() {
             startActivity(intent)
         }
         renderCommentUi(commentEditText)
+
+
+
+
+    }
+
+
+    private fun renderWebView(posting_index : Int){
+        var retrofit = RetrofitClient.initClient()
+        var sendData = retrofit.create(RetrofitClient.GetSpecificPostApi::class.java)
+        sendData.getPost(posting_index).enqueue(object : retrofit2.Callback<ClickedPostData>{
+            override fun onFailure(call: Call<ClickedPostData>, t: Throwable) {
+                Toast.makeText(this@AnswerActivity, "서버 연결이 불안정합니다", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<ClickedPostData>,
+                response: Response<ClickedPostData>
+            ) {
+                Log.d("이거입니당", response.body()!!.own.toString())
+                Log.d("이거입니당", response.body()!!.posting.toString())
+                askPostWebView.loadUrl(askWebViewUrl)
+                askPostWebView.loadUrl("javascript:getPost('asdf')")
+            }
+
+        })
     }
 
     private fun renderCommentUi(commentEditText : EditText) {
